fe2a2016a9db2888d5cc561dc21d5c09fef50b5f
Fernando Garcia <fergarciadlc@gmail.com>
2025-05-14 15:50:23 +0200

    format plugin files

--- plugin/include/SallenKey/PluginEditor.h
+++ plugin/include/SallenKey/PluginEditor.h
@@ -5,18 +5,18 @@
 //==============================================================================
 class AudioPluginAudioProcessorEditor final : public juce::AudioProcessorEditor
 {
-public:
-    explicit AudioPluginAudioProcessorEditor (AudioPluginAudioProcessor&);
+  public:
+    explicit AudioPluginAudioProcessorEditor(AudioPluginAudioProcessor&);
     ~AudioPluginAudioProcessorEditor() override;
 
     //==============================================================================
-    void paint (juce::Graphics&) override;
+    void paint(juce::Graphics&) override;
     void resized() override;
 
-private:
+  private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     AudioPluginAudioProcessor& processorRef;
 
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (AudioPluginAudioProcessorEditor)
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(AudioPluginAudioProcessorEditor)
 };
