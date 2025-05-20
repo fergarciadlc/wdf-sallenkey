908710865346016351cba4e0e362e4cfb72eb0ac
Fernando Garcia <fergarciadlc@gmail.com>
2025-05-20 22:05:53 +0200

    Implement 2nd order LPF with cascade

--- plugin/include/WDFilters/PluginProcessor.h
+++ plugin/include/WDFilters/PluginProcessor.h
@@ -7,34 +7,6 @@
 
 //==============================================================================
 
-namespace wdft = chowdsp::wdft;
-
-struct RCLowpass
-{
-    wdft::ResistorT<double>  r1{1.0e3};  // 1 KOhm Resistor
-    wdft::CapacitorT<double> c1{1.0e-6}; // 1 uF capacitor
-
-    wdft::WDFSeriesT<double, decltype(r1), decltype(c1)> s1{r1,
-                                                            c1}; // series connection of r1 and c1
-    // Use double precision to match WDF types and avoid implicit conversion warnings
-    wdft::PolarityInverterT<double, decltype(s1)>   i1{s1}; // invert polarity
-    wdft::IdealVoltageSourceT<double, decltype(s1)> vs{s1}; // input voltage source
-
-    // prepare the WDF model here...
-    void prepare(double sampleRate) { c1.prepare(sampleRate); }
-
-    // use the WDF model to process one sample of data
-    inline double processSample(double x)
-    {
-        vs.setVoltage(x);
-
-        vs.incident(i1.reflected());
-        i1.incident(vs.reflected());
-
-        return wdft::voltage<double>(c1);
-    }
-};
-
 class AudioPluginAudioProcessor final : public juce::AudioProcessor
 {
 public:
@@ -75,7 +47,8 @@ public:
     void setStateInformation(const void* data, int sizeInBytes) override;
 
 private:
-    WDFRCLowPass filter;
+    // WDFRCLowPass filter;
+    WDFRC2Cascade filter;
     //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(AudioPluginAudioProcessor)
 };
